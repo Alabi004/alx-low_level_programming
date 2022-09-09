6462338587c666eafcdf62abd00ec6437352ce4c
Alabi004 <alabirokeeb2020@gmail.com>
2022-09-08 19:26:44 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	print("Size of an int: %d byte(s)\n", sizeof(a);
+	print("Size of a long int: %d byte(s)\n", sizeof(b);
+	print("Size of a long long int: %d byte(s)\n", sizeof(c);
+	print("Size of a char: %d byte(s)\n", sizeof(d);
+	print("Size of a float: %d byte(s)\n", sizeof(e);
+	return (0);
+}
