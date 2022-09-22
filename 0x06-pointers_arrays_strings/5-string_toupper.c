6f3535191915aef38b266782032e96043bd2a3de
Alabi004 <alabirokeeb2020@gmail.com>
2022-09-22 06:49:05 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,15 @@
  * Return: address tp the string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-	int i = 0;
+	int index = 0;
 
-	while (str[i] != '\0')
+	while (str[index] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-		i++;
+		if (str[index] >= 'a' && str[i] <= 'z')
+			str[index] -= 32;
+		index++;
 	}
 	return (str);
 }
