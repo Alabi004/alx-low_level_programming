797665ccf288fc9c0a09d131e40ab29e23bbcbbd
Alabi004 <alabirokeeb2020@gmail.com>
2022-09-21 23:34:06 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen -returs the length of a string
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+* cap_string - function that capitalize firt character og a word
+* @str: string to capitalize
+*Return: returns the capitalizes string
+*/
+
+clear
