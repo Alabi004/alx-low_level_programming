0fe815a4b847018a159763562b1fc1b6bb2ddb80
Alabi004 <alabirokeeb2020@gmail.com>
2022-09-23 08:57:58 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -23,8 +20,8 @@ char *rot13(char *);
 
 void print_number(int n);
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
-
-#endif
