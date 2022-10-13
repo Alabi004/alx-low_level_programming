87b5c6ec33b8dbfce623ac75e942933a8fbcc229
Alabi004 <alabirokeeb2020@gmail.com>
2022-10-13 15:14:10 -0700

    correct 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,14 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	if (!array || !action)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+size_t i;
+if (array == NULL || action == NULL)
+{
+return;
 }
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+
