388aac8e52898c958f473129e14bfcb2e1c32956
Alabi004 <alabirokeeb2020@gmail.com>
2022-10-14 15:58:31 -0700

    cm

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,13 @@
  * @n: amount of the arguments.
  *
  * Return: sum of its parameters.
-**/
-
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
 	int sum = 0;
-	
+
 	if (n == 0)
 		return (0);
 
